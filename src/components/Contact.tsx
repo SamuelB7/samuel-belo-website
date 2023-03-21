6fcc482a6a47d7c4b738bee7df82999aa45e41ff
SamuelBelo7 <samuel.belo@seteloc.com.br>
2023-03-20 22:03:49 -0300

    form submit added and thank you page added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,35 +4,38 @@ import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
 
 function Contact() {
   return (
-    <div id='contact' className='w-full'>
+    <div id='contact' className='w-full my-5'>
         <div className='max-w-7xl m-auto p-4 w-full h-full'>
-            <h2 className=''>Contact me</h2>
-            <form action="">
+            <h2 className=''>Get in contact</h2>
+            <form action="https://formsubmit.co/belo.samuel@gmail.com" method='POST'>
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                     <div className='flex flex-col'>
                         <label className='text-sm py-2'>NAME</label>
-                        <input className='border-2 rounded-lg p-3 flex border-gray-300 text-slate-700' type="text" />
+                        <input className='border-2 rounded-lg p-3 flex border-gray-300 text-slate-700' type="text" name='name'/>
                     </div>
                     <div className='flex flex-col'>
                         <label className='text-sm py-2'>EMAIL</label>
-                        <input className='border-2 rounded-lg p-3 flex border-gray-300 text-slate-700' type="email" />
+                        <input className='border-2 rounded-lg p-3 flex border-gray-300 text-slate-700' type="email" name='email'/>
                     </div>
                 </div>
                 <div className='flex flex-col py-2'>
                     <label className='text-sm py-2'>MESSAGE</label>
-                    <textarea className='border-2 rounded-lg p-3 border-gray-300 text-slate-700' rows={10}></textarea>
+                    <textarea className='border-2 rounded-lg p-3 border-gray-300 text-slate-700' rows={10} name='message'></textarea>
                 </div>
-                <button className='w-full p-4 text-gray-100 mt-4'>SEND MESSAGE</button>
+                <input type="hidden" name="_captcha" value="false"></input>
+                <input type="hidden" name="_subject" value="Contato do website pessoal"></input>
+                <input type="hidden" name="_next" value="https://samuelbelo.com.br/thankyou"></input>
+                <button type='submit' className='w-full p-4 text-gray-100 bg-gradient-to-r from-slate-300 to-slate-600 mt-4'>SEND MESSAGE</button>
             </form>
         </div>
-        <div className='flex justify-center py-12'>
+        {/* <div className='flex justify-center py-12'>
             <Link href='/#home' scroll={false}>
                 <div className='rounded-full border border-white p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
                     <HiOutlineChevronDoubleUp size={30}/>
                 </div>
             </Link>
 
-        </div>
+        </div> */}
     </div>
   )
 }
---
+++ src/pages/thankyou.tsx
@@ -0,0 +1,59 @@
+import Navbar from '@/components/Navbar'
+import Link from 'next/link'
+import React from 'react'
+import { FaGithub, FaLinkedinIn, FaWhatsapp } from 'react-icons/fa'
+import { HiOutlineDocumentText } from 'react-icons/hi'
+
+function ThankYou() {
+    return (
+        <>
+            <Navbar />
+            <div id='home' className='w-full h-screen text-center'>
+                <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+                    <div>
+                        <h1 className='py-2'>Thanks for your message</h1>
+                        <h2 className='py-2'>Connect with me</h2>
+                        <div className='flex items-center justify-between max-w-xs m-auto py-4'>
+                            <div className='rounded-full border border-white p-3 cursor-pointer hover:scale-105 ease-in duration-300 group flex relative'>
+                                <Link href="https://www.linkedin.com/in/samuelb7/" target={'_blank'}>
+                                    <FaLinkedinIn />
+                                </Link>
+                                <span className="group-hover:opacity-100 transition-opacity bg-slate-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                                    LinkedIn
+                                </span>
+                            </div>
+                            <div className='rounded-full border border-white p-3 cursor-pointer hover:scale-105 ease-in duration-300 group flex relative'>
+                                <Link href="https://github.com/SamuelB7" target={'_blank'}>
+                                    <FaGithub />
+                                </Link>
+                                <span className="group-hover:opacity-100 transition-opacity bg-slate-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                                    GitHub
+                                </span>
+                            </div>
+                            <div className='rounded-full border border-white p-3 cursor-pointer hover:scale-105 ease-in duration-300 group flex relative'>
+                                <a href="documents/SAMUEL_BELO_Resume.pdf" download={"SAMUEL's Resume.pdf"}>
+                                    <HiOutlineDocumentText />
+
+                                </a>
+                                <span className="group-hover:opacity-100 transition-opacity bg-slate-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                                    Resume
+                                </span>
+
+                            </div>
+                            <div className='rounded-full border border-white p-3 cursor-pointer hover:scale-105 ease-in duration-300 group flex relative'>
+                                <Link href="https://wa.me/554198834077" target={'_blank'}>
+                                    <FaWhatsapp />
+                                </Link>
+                                <span className="group-hover:opacity-100 transition-opacity bg-slate-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                                    WhatsApp
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ThankYou
